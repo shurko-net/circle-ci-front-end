76c7d2a38188da6bc480285eba30ccafc82ea7fd
Yaroslav <ytube.noobtv@gmail.com>
2022-12-15 22:42:47 +0200

    Create media queries for NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -18,45 +18,108 @@ const NavBarWrapper = styled.div`
     }
 `;
 
-const Container = styled.div`
+const Body = styled.div`
   height: inherit;
   display:flex;
   justify-content: center;
-  align-items: center;
+  /* align-items: center; */
 `;
 
 const NavbarBrand = styled.div`
-  margin-left: 30%;
+  display: flex;
+  /* margin-left: 30%; */
   white-space:nowrap;
+  align-items: center;
+  @media (max-width: 730px) {
+    a:nth-child(-n+2) {
+    display: none;
+  }
+  @media (max-width: 550px) {
+    a:nth-child(-n+3) {
+    display: none;
+    }
+  }
+}
+`;
+
+const Conteiner = styled.div`
+  min-width: 0;
+  max-width: 1192px;
+  width: 100%;
+  @media (min-width: 1080px) {
+    margin: 0 64px;
+  }
+  @media (min-width: 904px) and (max-width: 1079.98px) {
+    margin: 0 64px;
+  }
+  @media (min-width: 728px) and (max-width: 903.98px) {
+    margin: 0 48px;
+  }
+  @media (max-width: 551.98px) {
+    margin: 0 24px;
+  }
+  @media (min-width: 552px) and (max-width: 727.98px){
+    margin: 0 24px;
+  }
+  @media (max-width: 551.98px){
+    margin: 0 24px;
+  }
+  
+
+`;
+
+const Content = styled.div`
+      flex-direction: row;
+      /* height: 75px; */
+      padding: 18.5px 0;
+      display: flex;
+      justify-content: space-between;
+`;
+
+const Block = styled.div`
+  display: block;
+`;
+
+const NavBlock = styled.div`
+  /* display: block; */
+  flex: 1 0 auto;
 `;
 
 function NavBar() {
   const [modalActive, setModalActive] = useState(false);
   return (
     <NavBarWrapper>
-      <Container>
-        <Link to="/">
-          <Text color="#131515" weight={700} fSize="33">
-            <div className="navbar-text">
-              Circle CI
-            </div>
-          </Text>
-        </Link>
-        <NavbarBrand>
-          <Link to="/about">
-            <Button>
-              Our story
-            </Button>
-          </Link>
-          <Button>
-            Membership
-          </Button>
-          {localStorage.getItem('login') && localStorage.getItem('password') && (
-            <Button>
-              Write
-            </Button>
-          )}
-          {!localStorage.getItem('login') && !localStorage.getItem('password')
+      <Body>
+        <Conteiner>
+          <Content>
+            <Link to="/">
+              <Block>
+                <Text color="#131515" weight={700} fSize="33">
+                  <div className="navbar-text">
+                    Circle CI
+                  </div>
+                </Text>
+              </Block>
+
+            </Link>
+            <NavBlock />
+            <NavbarBrand>
+              <Link to="/about">
+                <Button>
+                  Our story
+                </Button>
+              </Link>
+              <Link to="/member">
+                <Button>
+                  Membership
+                </Button>
+              </Link>
+              {localStorage.getItem('login') && localStorage.getItem('password') && (
+              <Button>
+                Write
+              </Button>
+              )}
+              {!localStorage.getItem('login') && !localStorage.getItem('password')
         && (
         <>
           <Link to="/login">
@@ -71,9 +134,11 @@ function NavBar() {
           </Link>
         </>
         )}
-          {modalActive && <Modal active={modalActive} setActive={setModalActive} />}
-        </NavbarBrand>
-      </Container>
+              {modalActive && <Modal active={modalActive} setActive={setModalActive} />}
+            </NavbarBrand>
+          </Content>
+        </Conteiner>
+      </Body>
     </NavBarWrapper>
   );
 }
